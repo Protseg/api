0d5f3e4f8d146e56fd51793b23a4624c04eae3ea
Claudio Holanda Jr <claudiohw@gmail.com>
2024-03-26 23:58:52 -0300

    fix: mysql connection closing

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,4 +6,7 @@ export const sql = await mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  maxIdle: 0,
+  idleTimeout: 60000,
+  enableKeepAlive: true,
 });
